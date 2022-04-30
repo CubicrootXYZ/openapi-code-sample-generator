359cbdc65f2f1e9d95d24b3102bb88cf62278a86
cubicroot <github@cubicroot.xyz>
2022-04-30 16:04:56 +0200

    clean up stuff

--- internal/languages/js.go
+++ internal/languages/js.go
@@ -26,17 +26,17 @@ func NewJS(encoders map[string]types.Encoder, extractor types.Extractor) types.G
 }
 
 // GetSample returns a js sample for the given operation
-func (p *JS) GetSample(httpVerb string, path string, operation *openapi3.Operation, pathItem *openapi3.PathItem, document *openapi3.T) (*types.CodeSample, error) {
-	p.usesToken = false
+func (js *JS) GetSample(httpVerb string, path string, operation *openapi3.Operation, pathItem *openapi3.PathItem, document *openapi3.T) (*types.CodeSample, error) {
+	js.usesToken = false
 	codeInit := strings.Builder{} // js variable definitions
-	codeExec := strings.Builder{} // js curl statement
+	codeExec := strings.Builder{} // js code
 
-	parameters, err := p.extractor.GetParameters(operation.Parameters)
+	parameters, err := js.extractor.GetParameters(operation.Parameters)
 	if err != nil {
 		return nil, err
 	}
 
-	secParameters, basicAuth, err := p.extractor.GetSecurity(operation, document)
+	secParameters, basicAuth, err := js.extractor.GetSecurity(operation, document)
 	if err != nil {
 		return nil, err
 	}
@@ -46,22 +46,22 @@ func (p *JS) GetSample(httpVerb string, path string, operation *openapi3.Operati
 	parameters.Path = append(parameters.Path, secParameters.Path...)
 	parameters.Cookie = append(parameters.Cookie, secParameters.Cookie...)
 
-	body, _ := p.getRequestBody(operation)
-	body = p.filterToken(body)
+	body, meta := js.getRequestBody(operation)
+	body = js.filterToken(body)
 
 	// Set the url
 	codeExec.WriteString("request.open(\"")
 	codeExec.WriteString(httpVerb)
 	codeExec.WriteString("\", \"")
-	codeExec.WriteString(p.extractor.GetURL(operation, pathItem, document))
-	codeExec.WriteString(p.extractor.GetPathExample(path, parameters.Path))
+	codeExec.WriteString(js.extractor.GetURL(operation, pathItem, document))
+	codeExec.WriteString(js.extractor.GetPathExample(path, parameters.Path))
 	if len(parameters.Query) > 0 {
 		codeInit.WriteString("?")
-		codeInit.WriteString(p.getQueryParams(parameters.Query))
+		codeInit.WriteString(js.getQueryParams(parameters.Query))
 	}
 	codeExec.WriteString(", false);\"\n")
 
-	if p.usesToken {
+	if js.usesToken {
 		codeInit.WriteString("var token = \"my secret token\"; // Put your token here\n")
 	}
 
@@ -70,7 +70,7 @@ func (p *JS) GetSample(httpVerb string, path string, operation *openapi3.Operati
 		codeExec.WriteString("request.setRequestHeader(\"")
 		codeExec.WriteString(header.Name)
 		codeExec.WriteString("\", ")
-		codeExec.WriteString(strings.TrimSuffix(strings.TrimPrefix("\""+p.filterToken(fmt.Sprint(header.Value)+"\""), "\"\" + "), " + \"\""))
+		codeExec.WriteString(strings.TrimSuffix(strings.TrimPrefix("\""+js.filterToken(fmt.Sprint(header.Value)+"\""), "\"\" + "), " + \"\""))
 		codeExec.WriteString(");\n")
 	}
 
@@ -78,6 +78,16 @@ func (p *JS) GetSample(httpVerb string, path string, operation *openapi3.Operati
 		codeInit.WriteString("var username = \"my username\";\n")
 		codeInit.WriteString("var password = \"*******\";\n")
 		codeExec.WriteString("request.setRequestHeader(\"Authorization\", \"Basic \" + btoa(username + \":\" + password));\n")
+	}
+
+	if meta.Format != "" {
+		codeExec.WriteString("request.setRequestHeader(\"Content-Type\", \"")
+		codeExec.WriteString(meta.Format)
+		if meta.FormData.OuterBoundary != nil {
+			codeExec.WriteString(" boundary=")
+			codeExec.WriteString(*meta.FormData.OuterBoundary)
+		}
+		codeExec.WriteString("\");\n")
 	}
 
 	// Set request body
@@ -124,77 +134,6 @@ func (p *JS) getQueryParams(params []*types.Parameter) string {
 	return query.String()
 }
 
-func (p *JS) getHeaderParams(params []*types.Parameter, meta *types.FormattingMeta) string {
-	head := strings.Builder{}
-	encoder, ok := p.encoders[types.EncodingWwwUrlencode]
-
-	if !ok {
-		log.Warn("Missing encoder for format: " + types.EncodingWwwUrlencode)
-		return "array()"
-	}
-
-	head.WriteString("array(\n")
-
-	for _, param := range params {
-		if param == nil {
-			continue
-		}
-
-		value, err := encoder.EnocdeValue("", param.Value, nil)
-		if err != nil {
-			log.Info(fmt.Sprintf("Skipped header parameter %s due to: %s", param.Name, err.Error()))
-			continue
-		}
-
-		head.WriteString("\t\"")
-		head.WriteString(p.escape(param.Name))
-		head.WriteString(": ")
-		head.WriteString(p.escape(value))
-		head.WriteString("\",\n")
-	}
-
-	encoding := p.writeFormatMeta(meta)
-
-	if encoding != "" {
-		head.WriteString("\t\"")
-		head.WriteString(encoding)
-		head.WriteString("\",\n")
-	}
-
-	head.WriteString(")")
-
-	// TODO handle ${TOKEN} also in cookie and params
-
-	return head.String()
-}
-
-func (p *JS) getCookieParams(params []*types.Parameter) string {
-	cookie := strings.Builder{}
-	encoder, ok := p.encoders[types.EncodingWwwUrlencode]
-
-	if !ok {
-		log.Warn("Missing encoder for format: " + types.EncodingWwwUrlencode)
-		return ""
-	}
-
-	for _, param := range params {
-		if param == nil {
-			continue
-		}
-
-		value, err := encoder.EnocdeParameter(param.Name, param.Value)
-		if err != nil {
-			log.Info(fmt.Sprintf("Skipped cookie parameter %s due to: %s", param.Name, err.Error()))
-			continue
-		}
-
-		cookie.WriteString(p.escape(value))
-		cookie.WriteString(";")
-	}
-
-	return cookie.String()
-}
-
 func (p *JS) getRequestBody(operation *openapi3.Operation) (string, *types.FormattingMeta) {
 	meta := &types.FormattingMeta{}
 	if operation.RequestBody == nil || operation.RequestBody.Value == nil {
@@ -238,26 +177,6 @@ func (p *JS) escape(text string) string {
 	text = strings.ReplaceAll(text, "\r\n", "\\r\\n")
 
 	return text
-}
-
-func (p *JS) writeFormatMeta(meta *types.FormattingMeta) string {
-	if meta == nil {
-		return ""
-	}
-
-	code := strings.Builder{}
-
-	if meta.Format != "" {
-		code.WriteString("Content-Type: ")
-		code.WriteString(meta.Format)
-
-		if meta.FormData.OuterBoundary != nil {
-			code.WriteString(" boundary=")
-			code.WriteString(*meta.FormData.OuterBoundary)
-		}
-	}
-
-	return code.String()
 }
 
 func (p *JS) filterToken(text string) string {
