f7f012fa9e4d413cbafcd2a27bde04e5611b06b0
cubicroot <github@cubicroot.xyz>
2026-02-15 17:47:33 +0100

    feat(encoding): :children_crossing: use same boundaries for form/data encoding

--- internal/encoding/form_data.go
+++ internal/encoding/form_data.go
@@ -25,6 +25,15 @@ func (f *FormDataEncode) EnocdeValue(ref string, value interface{}, meta *types.
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
 
+	if meta != nil {
+		// New endpoint, reset boundaries to use the same ones across endpoints.
+		resetBoundaries()
+	}
+	err := writer.SetBoundary(getNextBoundary())
+	if err != nil {
+		return "", err
+	}
+
 	if newValue, ok := value.(map[interface{}]interface{}); ok {
 		for key, value := range newValue {
 			f.writePart(fmt.Sprint(key), value, writer)
@@ -81,4 +90,42 @@ func (f *FormDataEncode) writePart(key string, value interface{}, writer *multip
 	} else {
 		_, _ = fmt.Fprint(part, value)
 	}
+}
+
+var currentBoundaryIndex = -1
+var boundaries = []string{
+	"ueng3raexexiFah9xae6keejie1oac7aid8thoe2aineiw4ohcip6aef4kee",
+	"oviongienesei6eel0uqueipungaixaecoh4wiezighahPh9aiShiokahy6h",
+	"iesh2FoonaloPhic0wohs8aey7thie0tuofahy8ahsh1unaeQueifohmoh5i",
+	"ul9ni9is2aiPhie7comejaW7eeb2sheekaaP9na2UXaPuh4pez9kein0aiw1",
+	"eniiBeedaefiu2eeh9aiG6ool8cheequ8the0raipeeniuP9ja8ohPhooSu8",
+	"ootaigiephei8avupuik5theida9Za8ezoB9ung7bee9Rieyauf6Ech5Chuo",
+	"oongie9phuyooTh8aeyaiC8eequaigh3ge1paeth9vataok7Vidaimeiquoo",
+	"inoo6op7itee0iuch8uecoipoChai4iexu5ex9kieshaithohae0jozaipee",
+	"Aehieju8aiziTh8iigh7rai4ainahchae3ocheehud2kohreexo6ooxei8Sa",
+	"Aek3eij4cei8Eehee9eemee7roPh0ahX6aim9Shaitho6vi7vee9phah5ahN",
+	"aidi1iudeichie8au7aizeowaiYail8ugh7aino8ohtoishai7eizailahGh",
+	"Shailahvie2ok9aigh8Huus7siet6weeshein4Ii5puoThoh8kaek4no5rie",
+	"aPhooveucheireiQuooxohpha3Oshohthae8ka4Oosie4egh0goos9cisooH",
+	"eeQuaphieb4eis6OhngooBil0le8Auw4gaeyooth3oomi1aiheiphaemae5h",
+	"shooNah7iejiChahth1koon6soqu8revopo7ooch6utayedouN7aeshoo3oa",
+	"daeng2koo0ma2Keisuy7peirecaiGhee6oSho6oojaeJaechoo1bielohvah",
+	"ahGee5ca9maej8jiesahph6eiGa9aPh2Eequuvoh2xukuu7Faed0aePh0ieZ",
+	"Ahnoo9Ixoo1id6Ohth7ief3neiChoh1webokahv4eeM3Bieshi2LeeW3Roka",
+	"Eetu2Eephie7EiJ0du0aeNohZ7ai3aiwi7gue2xahhodaJaht9xuquushieh",
+	"ia7aiGh1ieL5jei8aex4pe9zee0quairo6te3eijaighiek3phehaith0oth",
+}
+
+// getNextBoundary is a workaround to use the same boundaries across examples.
+func getNextBoundary() string {
+	currentBoundaryIndex++
+	if currentBoundaryIndex >= len(boundaries) {
+		currentBoundaryIndex = 0
+	}
+
+	return boundaries[currentBoundaryIndex]
+}
+
+func resetBoundaries() {
+	currentBoundaryIndex = -1
 }
